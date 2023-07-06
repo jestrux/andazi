7ff2e6fb2b849a423ede70f2c48af4a8265edd48
Walter Kimaro <wakyj07@gmail.com>
2023-07-06 04:36:30 +0300

    Fix puase icon

--- src/ProjectDetail/index.jsx
+++ src/ProjectDetail/index.jsx
@@ -115,7 +115,7 @@ const ProjectDetail = () => {
 						className="mb-0.5 mx-3 flex h-12 aspect-square rounded-full shadow-lg bg-white/5 border border-neutral-200/50 dark:border-white/5 items-center justify-center"
 						onClick={handlePlay}
 					>
-						<svg className="ml-0.5 mt-px h-6" viewBox="0 0 24 24">
+						<svg className={`${!playing && 'ml-0.5'} mt-px h-6`} viewBox="0 0 24 24">
 							<defs>
 								<linearGradient
 									id="grad1"
@@ -144,7 +144,7 @@ const ProjectDetail = () => {
 							<path
 								fillRule="evenodd"
 								d={
-									playing > 0
+									playing
 										? "M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
 										: "M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
 								}
